1636fa15374bf91b917ca33a856e0702f3ebb42c
Etienne Marais <dev@maiste.fr>
2024-08-02 11:40:11 +0200

    tmp: config for test on the server

--- sandworm/bin/config.ml
+++ sandworm/bin/config.ml
@@ -1,4 +1,4 @@
-let s3_bucket_ref = "dune-binary-distribution:/dune/"
+let s3_bucket_ref = "dune-binary-distribution:/dune/test"
 let s3_public_url = "https://download.dune.ci.dev"
 let artifacts_path = "./artifacts"
 let metadata_file = "./metadata.json"
